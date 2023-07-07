95a6427b04f62b5d434acae3edadd7290f0f65ba
Ariel Junior <act117@einstein.br>
2023-07-06 21:46:27 -0300

    menu e submenu consulta cliente

--- principal.py
+++ principal.py
@@ -1,1 +1,6 @@
-print("hello world")
+from controllers.menu import iniciar_menu_principal
+from datetime import datetime
+
+print(f"\n{datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}\n")
+
+iniciar_menu_principal()
